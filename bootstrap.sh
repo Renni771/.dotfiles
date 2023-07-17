e0d63e0c7654efea94bf9bc5136f0ef62cf8c283
Rendani Gangazhe <rendanig@gmail.com>
2023-07-17 23:16:30 +0200

    Add basic bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+# link files
+CONFIG_DIR=$HOME/.config
+
+ln -s $HOME/.dotfiles/nvim/ $CONFIG_DIR
+
+ln -s $HOME/.dotfiles/zsh/ $CONFIG_DIR
+ln -s $CONFIG_DIR/zsh/.zshrc $HOME
+ln -s $CONFIG_DIR/zsh/.zshenv $HOME
+
+ln -s $HOME/.dotfiles/alacritty/ $CONFIG_DIR
+ln -s $HOME/.dotfiles/.tmux.conf $HOME
+
